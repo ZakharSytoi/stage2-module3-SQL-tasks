02f2468109456d36ed528a958296c70f5b2a50d1
ZakharSytoi <zakharsytoi@gmail.com>
2023-05-11 19:15:36 +0200

    better solution

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,39 +1,43 @@
-CREATE TABLE Student
+
+CREATE TABLE student
 (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(64),
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45),
     birthday DATE,
-    group_ INT
+    groupnumber INT NOT NULL
+);
+CREATE TABLE subject
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250),
+    description VARCHAR(255),
+    grade int NOT NULL
 );
 
-CREATE TABLE Subject(
-                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                        name VARCHAR(64),
-                        description VARCHAR(512),
-                        grade INT
+CREATE TABLE mark
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    student_id BIGINT,
+    subject_id BIGINT,
+    mark INT NOT NULL,
+    foreign key (student_id) references student(id),
+    foreign key (subject_id) references subject(id)
 );
 
-CREATE TABLE PaymentType(
-                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                            name VARCHAR(64)
+CREATE TABLE paymenttype
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45)
 );
 
-CREATE TABLE Payment(
-                        id BIGINT AUTO_INCREMENT PRIMATY KEY,
-                        type_id BIGINT,
-                        amount DECIMAL,
-                        student_id BIGINT,
-                        payment_date DATETIME,
-                        FOREIGN KEY (type_id) REFERENCES PaymentType(id),
-                        FOREIGN KEY (student_id) REFERENCES Student(id)
-);
-
-CREATE TABLE Mark(
-                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                     student_id BIGINT,
-                     subject_id BIGINT,
-                     mark INT,
-                     FOREIGN KEY (Student_id) REFERENCES Student(id),
-                     FOREIGN KEY (subject_id) REFERENCES Subject(id)
+CREATE TABLE payment
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    type_id BIGINT,
+    amount decimal NOT NULL,
+    payment_date TIMESTAMP,
+    student_id BIGINT NOT NULL,
+    foreign key (type_id) references paymenttype(id),
+    foreign key (student_id) references student(id)
 );
 
