b3f23107190e1ad63dd67fdd939808ea32db5c9c
ZakharSytoi <zakharsytoi@gmail.com>
2023-05-14 15:05:38 +0200

    lesson 12 TABLES_CHANGES fixed

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,19 @@
-select * from student;
+ALTER TABLE mark
+drop CONSTRAINT mark_student_id_fkey;
+
+ALTER TABLE mark
+    ADD CONSTRAINT mark_student_id_fkey_on_delete
+        foreign key (student_id)
+            references student(id)
+            ON DELETE CASCADE;
+
+
+ALTER TABLE payment
+drop CONSTRAINT payment_student_id_fkey;
+
+ALTER TABLE payment
+    ADD CONSTRAINT payment_student_id_fkey_on_delete
+        foreign key (student_id)
+            references student(id)
+            ON DELETE CASCADE;
+
