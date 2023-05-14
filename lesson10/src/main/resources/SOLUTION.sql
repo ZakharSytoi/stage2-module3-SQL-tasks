18b59257df62243f1d947f1d7f70925c842c6a45
ZakharSytoi <zakharsytoi@gmail.com>
2023-05-14 13:15:38 +0200

    lesson 10 completed

---
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -0,0 +1,13 @@
+SELECT s.*
+FROM subject AS s JOIN mark AS m ON s.id = m.subject_id
+GROUP BY s.id
+HAVING AVG(mark) >
+       (SELECT AVG(mark)
+        FROM subject AS s JOIN mark AS m ON s.id = m.subject_id );
+
+SELECT s.*
+FROM student AS s JOIN payment AS p ON s.id = p.student_id
+GROUP BY s.id
+HAVING AVG(amount) <
+        (SELECT AVG(amount)
+        FROM student AS s JOIN payment AS p ON s.id = p.student_id);
